d8e3b4cdfa88487ad3a04ef84d9b6a8c0fb7e78b
Martin Daniel <martindaniel4@gmail.com>
2022-01-24 11:13:58 +0100

    add energy savings

--- heater.py
+++ heater.py
@@ -4,7 +4,7 @@ import plotly.express as px
 import statsmodels.formula.api as smf
 from temperature.exterior import get_ext_df
 
-PATH_TEMP = {'interior': 'netatmo/export/netatmo_temperature_2020-01-01_2022-01-15.csv',
+PATH_TEMP = {'interior': 'netatmo/export/netatmo_temperature_2020-01-01_2022-01-23.csv',
              'exterior': 'temperature/export/daily_export'}
 PATH_GAZ = "gaz/data/gaz_conso.csv"
 DATE_NEW_ROOF = '2021-07-18'
@@ -58,21 +58,34 @@ def stack_temperature(PATH_TEMP):
     return df
 
 
-def merge_gaz_temperature(PATH_GAZ, PATH_TEMP):
+def _season(x):
+    """
+    Seasonal function.
+    """
+    if x.month in [1, 2, 3, 4, 10, 11, 12]:
+        return 'winter'
+    else:
+        return 'summer'
+
+
+def merge_gaz_temperature(PATH_GAZ, PATH_TEMP, season='winter'):
     """
     Merge gaz and temperature.
     """
     gaz = read_gaz(PATH_GAZ)
     temp = read_temperature(PATH_TEMP)
     df = pd.merge(gaz, temp, on='date', how='left')
+    df['season'] = df['date'].apply(_season)
+    if season == 'winter':
+        df = df.query("season == 'winter'")
     return df
 
 
-def build_energy_model(PATH_GAZ, PATH_TEMP):
+def build_energy_model(PATH_GAZ, PATH_TEMP, season):
     """
     Build energy model.
     """
-    df = merge_gaz_temperature(PATH_GAZ, PATH_TEMP)
+    df = merge_gaz_temperature(PATH_GAZ, PATH_TEMP, season)
     df = df.query("date < '{}'".format(DATE_NEW_ROOF))
     model = smf.ols(formula='energy ~ temperature + temperature_ext', data=df)
     results = model.fit()
@@ -80,9 +93,9 @@ def build_energy_model(PATH_GAZ, PATH_TEMP):
     return results
 
 
-def predict_on_set(PATH_GAZ, PATH_TEMP, type='test'):
-    model = build_energy_model(PATH_GAZ, PATH_TEMP)
-    df = merge_gaz_temperature(PATH_GAZ, PATH_TEMP)
+def predict_on_set(PATH_GAZ, PATH_TEMP, season, type='test'):
+    model = build_energy_model(PATH_GAZ, PATH_TEMP, season)
+    df = merge_gaz_temperature(PATH_GAZ, PATH_TEMP, season)
     if type == 'test':
         df = df.query("date > '{}'".format(DATE_NEW_ROOF))
     df['energy_predicted'] = model.predict(
@@ -90,20 +103,37 @@ def predict_on_set(PATH_GAZ, PATH_TEMP, type='test'):
     return df
 
 
-def plot_prediction(PATH_GAZ, PATH_TEMP, type='test'):
+def plot_prediction(PATH_GAZ, PATH_TEMP, season, type='test'):
     """
     Plot predicted energy.
     """
-    df = predict_on_set(PATH_GAZ, PATH_TEMP, type)
+    df = predict_on_set(PATH_GAZ, PATH_TEMP, season, type)
     df.set_index('date', inplace=True)
     df_stack = df[['energy', 'energy_predicted']].stack().reset_index()
     df_stack.columns = ['date', 'type', 'value']
     fig = px.line(df_stack, x='date', y='value', color='type')
     fig.show()
 
-# %%
+
+def compute_energy_savings(PATH_GAZ, PATH_TEMP, season='winter', type='all'):
+    """
+    What's the difference between the energy consumption and the energy predicted?
+    """
+    df = predict_on_set(PATH_GAZ, PATH_TEMP, season, type)
+    df['diff'] = df['energy'] - df['energy_predicted']
+    savings_since_roof = df.query(
+        "date > '{}'".format(DATE_NEW_ROOF))['diff'].sum()
+    print("Savings since roof: {} kwh".format(savings_since_roof))
+    df['month_year'] = df['date'].apply(lambda x: x.strftime('%m-%Y'))
+    savings = df.groupby('month_year').agg({'diff': 'sum'}).reset_index()
+    savings['date'] = pd.to_datetime(savings['month_year'], format="%m-%Y")
+    savings.sort_values('date', inplace=True)
+    return savings
+
+    # %%
 
 
+plot_prediction(PATH_GAZ, PATH_TEMP, season='winter', type='all')
 # %%
 
 # mod = smf.ols(formula='energy ~ boileron', data=merge_gaz_heater(heater, gaz))
@@ -121,6 +151,7 @@ def plot_prediction(PATH_GAZ, PATH_TEMP, type='test'):
 #            y='boileron',
 #            hover_data=['date'])
 # %%
+
 
 def merge_gaz_heater(heater, gaz):
     """
@@ -150,3 +181,5 @@ def plot_gaz_heater(heater, gaz):
     fig = px.bar(df, x='date', y='value', facet_row='type', color='type')
     fig.update_yaxes(matches=None)
     fig.show()
+
+# %%
